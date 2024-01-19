99f8975a97bbe51d624c38436b76d8eca7d379ec
Phan Vuong <pdvuongtb@gmail.com>
2024-01-19 17:38:17 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/qlmh-new/",
+  base: '/qlmh-new/',
   build: {
     chunkSizeWarningLimit: 1600,
   }
